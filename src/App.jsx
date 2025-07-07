c8a94d9f38ebebb937d8d7954fce25231e841fe4
akshay-ken <akshaybest3120@gmail.com>
2025-07-07 18:21:08 +0530

    adding theme directive

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 function App() {
   return (
     <>
-      <h1>working</h1>
+      <h1
+        className="bg-linear-to-r from-[hsl(249, 99%, 64%)]
+       to-[hsl(278, 94%, 30%)]"
+      >
+        working
+      </h1>
     </>
   );
 }
